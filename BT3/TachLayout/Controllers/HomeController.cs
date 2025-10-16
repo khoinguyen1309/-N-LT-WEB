c3e5154e668e72736ca8868ebb0191d8bd06b618
akaKiet <0306231399@caothang.edu.vn>
2025-10-16 22:09:26 +0700

    Kiệt thêm trang view Thong Tin

--- BT3/TachLayout/Controllers/HomeController.cs
+++ BT3/TachLayout/Controllers/HomeController.cs
@@ -117,5 +117,10 @@ namespace TachLayout.Controllers
             return View();
         }
 
+        public IActionResult ThongTin()
+        {
+            ViewData["Title"] = "ThongTin";
+            return View();
+        }
     }
 }
